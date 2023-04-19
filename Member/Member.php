eadb43740ee6386c18c82cb9c72c8e3f9519faa7
keiho <98437950+Nkeiho@users.noreply.github.com>
2023-04-19 13:50:59 +0900

    Add files via upload

---
+++ Member/Member.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" media="all" href="Member.css">
+</head>
+<body>
+<div class="header">
+    <div class="logo">
+        <img src="kyutech_logo.gif" alt="picture">
+        <h1>Nakatoh Lab<span>Kyushu Institute of Technology</span></h1>
+    </div>
+
+    <nav class="mainnav">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Member</a></li>
+            <li><a href="#">Achivement</a></li>
+            <li><a href="#">Event</a><aa/li>
+            <li><a href="#">Access</a></li>
+        </ul>
+    </nav>
+</div>
+
+<div class = "card-container">
+    <h1>Members<span>メンバー</span></h1>
+    <img src="member1.jpg" alt="picture">
+</div> 
+
+<div class = "main">
+    <h2>教員・スタッフ</h2>
+    <div class = "teacher">
+        <a href=#><img src="otani.jpeg" alt="picture"></a>
+        <p class="jp">中藤良久<span class="en">Yosihisa Nakatoh</span></p>
+        <a href=#><img src="otani.jpeg" alt="picture"></a>
+        <p class="jp">Yujie Li<span class="en">Vickey-Lee</span></p>
+        <a href=#><img src="otani.jpeg" alt="picture"></a>
+        <p class="jp">原田勝也<span class="en">Katsuya Harada</span></p>
+    </div>
+    <h2>学生</h2>  
+    <div class = "student">
+        <a href=#><img src="otani.jpeg" alt="picture"></a>
+        <p class="jp">原田勝也<span class="en">Katsuya Harada</span></p>
+        <a href=#><img src="otani.jpeg" alt="picture"></a>
+        <p class="jp">原田勝也<span class="en">Katsuya Harada</span></p>
+        <a href=#><img src="otani.jpeg" alt="picture"></a>
+        <p class="jp">原田勝也<span class="en">Katsuya Harada</span></p>
+        <a href=#><img src="otani.jpeg" alt="picture"></a>
+        <p class="jp">原田勝也<span class="en">Katsuya Harada</span></p>
+        <a href=#><img src="otani.jpeg" alt="picture"></a>
+        <p class="jp">原田勝也<span class="en">Katsuya Harada</span></p>
+        <a href=#><img src="otani.jpeg" alt="picture"></a>
+        <p class="jp">原田勝也<span class="en">Katsuya Harada</span></p>
+    </div>
+    <h2><a href="#">OB・OG</a></h2>
+    <div class="ob">
+        <p class="jp">原田勝也<span class="en">Katsuya Harada</span></p>
+    </div>
+ </div>
+
+<footer id="footer">
+</footer>
+
+</body>
+</html>
