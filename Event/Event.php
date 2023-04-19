3b455e45aefdf3c2d51b567d003552e0a093bdfc
keiho <98437950+Nkeiho@users.noreply.github.com>
2023-04-19 14:17:34 +0900

    Add files via upload

---
+++ Event/Event.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" media="all" href="Event.css">
+</head>
+<body>
+<div class="header">
+    <div class="logo">
+        <img src="kyutech_logo.gif" alt="picture">
+        <h1>Nakatoh Lab<span>Kyushu Institute of Technology</span></h1>
+    </div>
+
+    <nav class="mainnav">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Member</a></li>
+            <li><a href="#">Achivement</a></li>
+            <li><a href="#">Event</a><aa/li>
+            <li><a href="#">Access</a></li>
+        </ul>
+    </nav>
+</div>
+
+<div class = "card-container">
+    <h1>Event<span>イベント</span></h1>
+    <img src="member1.jpg" alt="picture">
+</div> 
+
+</body>
+</html>
